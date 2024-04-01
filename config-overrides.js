f095962beebb56f4ef910031f0206db0f2cdbba7
Nick Carducci <nmcarducci@gmail.com>
2024-04-01 12:57:05 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -9,6 +9,7 @@ module.exports = function override(config, env) {
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify/browser"),
+    path: require.resolve("path-browserify"),
     buffer: require.resolve("buffer"),
     stream: require.resolve("stream-browserify")
   };
