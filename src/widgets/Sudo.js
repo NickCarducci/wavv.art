54754b4b4e181d8d20548e1f0d1ce9f3e5ff664e
Nick Carducci <nmcarducci@gmail.com>
2024-04-02 11:13:27 -0400

    Update Sudo.js

--- src/widgets/Sudo.js
+++ src/widgets/Sudo.js
@@ -634,34 +634,6 @@ class FIREBASE_SUDO extends React.Component {
           {this.props.auth === undefined
             ? this.props.useTitle && this.props.useTitle
             : this.props.memberMessage}
-          {this.props.auth !== undefined && (
-            <div
-              style={{
-                position: "relative",
-                margin: "auto",
-                marginBottom: "4px",
-                width: "max-content",
-                borderTopRightRadius: "10px",
-                borderTopLeftRadius: "10px",
-                padding: "16px 4px",
-                backgroundColor: !this.props.backgroundColor
-                  ? "rgba(255,255,255,.6)"
-                  : "" //this.props.auth!==undefined.sausageadmin
-              }}
-            >
-              <span
-                style={{
-                  cursor: "pointer",
-                  border: "1px solid",
-                  borderRadius: "4px",
-                  padding: "10px 4px"
-                }}
-                onClick={() => this.props.logoutofapp()}
-              >
-                logout
-              </span>
-            </div>
-          )}
           {
             //this.state.showRecaptcha && (
             <div
