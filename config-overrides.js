eda93fbed696bca46e883fed93f017a6049e13a1
Nick Carducci <nmcarducci@gmail.com>
2024-04-03 21:46:09 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -11,7 +11,8 @@ module.exports = function override(config, env) {
     os: require.resolve("os-browserify/browser"),
     path: require.resolve("path-browserify"),
     buffer: require.resolve("buffer"),
-    stream: require.resolve("stream-browserify")
+    stream: require.resolve("stream-browserify"),
+    vm: require.resolve("vm-browserify")
   };
   config.plugins.push(
     new webpack.ProvidePlugin({
