266dd9474080bbea610e6c98b966f9845a4b25c9
Nick Carducci <nmcarducci@gmail.com>
2024-04-04 16:20:56 -0400

    Update local-photo.js

--- src/App/TypesMap/local-photo.js
+++ src/App/TypesMap/local-photo.js
@@ -276,37 +276,35 @@ class Composition extends React.Component {
             width: maxWidth
           }}
         >
-          {Photo ? (
-            Photo
-          ) : !Errorf ? (
-            <div id={id} style={{ width: "100%", position: "relative" }}>
-              {this.state.error && (
-                <button
-                  style={buttonStyle}
-                  onClick={() => {
-                    const { id, i, fallback, verbose } = this.props;
+          {Photo
+            ? Photo
+            : !Errorf && (
+                <div id={id} style={{ width: "100%", position: "relative" }}>
+                  {this.state.error && (
+                    <button
+                      style={buttonStyle}
+                      onClick={() => {
+                        const { id, i, fallback, verbose } = this.props;
 
-                    this.getPhotos(id, i, fallback, verbose);
-                    //this.storePhoto(fallback, id, i)
-                  }}
-                >
-                  restore photo
-                </button>
+                        this.getPhotos(id, i, fallback, verbose);
+                        //this.storePhoto(fallback, id, i)
+                      }}
+                    >
+                      restore photo
+                    </button>
+                  )}
+                  <div id={id} style={{ width: "100%", position: "relative" }}>
+                    <img
+                      id={id}
+                      //ref={this.Photo}
+                      alt={alt}
+                      className={className}
+                      src={fallback}
+                      style={{ ...lazyImageStyle, ...style }}
+                    />
+                  </div>
+                </div>
               )}
-              <div id={id} style={{ width: "100%", position: "relative" }}>
-                <img
-                  id={id}
-                  //ref={this.Photo}
-                  alt={alt}
-                  className={className}
-                  src={fallback}
-                  style={{ ...lazyImageStyle, ...style }}
-                />
-              </div>
-            </div>
-          ) : (
-            <div className="loader" />
-          )}
         </div>
       </div>
     );
